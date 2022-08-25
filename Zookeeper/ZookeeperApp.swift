70014954324a735673be1e3e58d785f33ee3c25d
PilarCuAr <tauro10cuen@gmail.com>
2022-08-25 14:19:21 -0500

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by MAC on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
